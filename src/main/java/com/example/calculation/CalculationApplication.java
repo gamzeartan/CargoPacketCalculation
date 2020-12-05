0aae8afe2075a3edb4c98c160f876ef844bf34f0
gamze artan <gamzeartn@gmail.com>
2020-12-05 23:25:12 +0300

    User inputs and console screen fixed.

--- src/main/java/com/example/calculation/CalculationApplication.java
+++ src/main/java/com/example/calculation/CalculationApplication.java
@@ -12,20 +12,50 @@ import org.apache.logging.log4j.Logger;
 @SpringBootApplication
 public class CalculationApplication {
 
-    //Using the getLogger method
+
+    static Scanner scanner = new Scanner(System.in);
+    static boolean var = true;
+
+
+    //using the getLogger method
     private static Logger log = LogManager.getLogger(CalculationApplication.class);
 
 
     public static void main(String[] args) {
 
-
         SpringApplication.run(CalculationApplication.class, args);
 
-        System.out.println("Hello World!!!");
-        log.info("log test!");
+        log.info("Log test.");
+
+        while(var){
+
+            System.out.println("Yapmak istediğiniz işlemi seçiniz..." +
+                    "\n1.Kargo Paketi Hesapla" +
+                    "\n2.Çıkış.");
+
+            int answer = scanner.nextInt();
+
+            switch(answer){
+
+                case 1:
+                    System.out.println("7 kilogramlık sabunların adedi:");
+                    int sevenKgSoapAmount = scanner.nextInt();
+                    System.out.println("2 kilogramlık sabunların adedi:");
+                    int twoKgSoapAmount = scanner.nextInt();
+                    System.out.println("İstenilen paket ağırlığı");
+                    int weightOfPacket = scanner.nextInt();
+                    break;
+
+                case 2:
+                    System.exit(0);
+                    break;
+
+
+            }
+
+        }
+
     }
-
-
 
 
 }
