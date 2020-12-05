96a1aef9ca8a17a46e4c251be588fac46f60ce55
gamze artan <gamzeartn@gmail.com>
2020-12-05 21:30:26 +0300

    Logging was added with log4j and log file was created. Dependencies added to pom.xml file.

--- src/main/java/com/example/calculation/CalculationApplication.java
+++ src/main/java/com/example/calculation/CalculationApplication.java
@@ -1,13 +1,31 @@
 package com.example.calculation;
-
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// importing log4j libraries
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 
 @SpringBootApplication
 public class CalculationApplication {
 
+    //Using the getLogger method
+    private static Logger log = LogManager.getLogger(CalculationApplication.class);
+
+
     public static void main(String[] args) {
+
+
         SpringApplication.run(CalculationApplication.class, args);
+
+        System.out.println("Hello World!!!");
+        log.info("log test!");
     }
+
+
+
 
 }
