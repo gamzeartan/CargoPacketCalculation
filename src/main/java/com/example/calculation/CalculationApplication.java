5ee0961da2b884fe2973a32525fa0aede0a79fde
gamze artan <gamzeartn@gmail.com>
2020-12-06 23:04:20 +0300

    bug fixes

--- src/main/java/com/example/calculation/CalculationApplication.java
+++ src/main/java/com/example/calculation/CalculationApplication.java
@@ -1,11 +1,15 @@
 package com.example.calculation;
+
+// importing Scanner class to get input from user
 import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // importing log4j libraries
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 
 
@@ -21,36 +25,44 @@ public class CalculationApplication {
     private static Logger log = LogManager.getLogger(CalculationApplication.class);
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         SpringApplication.run(CalculationApplication.class, args);
 
-        log.info("Log test.");
 
-        while(var){
+        while(var)
+        {
 
             System.out.println("Yapmak istediğiniz işlemi seçiniz..." +
                     "\n1.Kargo Paketi Hesapla" +
                     "\n2.Çıkış.");
 
-            int answer = scn.nextInt();
+            int answer = scn.nextInt(); //getting a selection from the user
+            log.info("Kulllanıcı işlem seçimi yaptı(1.Hesaplama,2.Çıkış): "+ answer);
 
-            switch(answer){
 
-                case 1:
+            switch(answer)
+            {
+
+                case 1: // for calculate the cargo package
+
                     System.out.println("7 kg'lık sabunların adedi: ");
-                    int sevenKgSoap = scn.nextInt();
-                    System.out.println("2 kg'lık sabunların adedi: ");
-                    int twoKgSoap = scn.nextInt();
-                    System.out.println("İstenilen paket ağırlığı: ");
-                    int weight = scn.nextInt();
+                    int sevenKgSoap = scn.nextInt(); //input data from the user
 
+                    System.out.println("2 kg'lık sabunların adedi: ");
+                    int twoKgSoap = scn.nextInt(); //input data from the user
+
+                    System.out.println("İstenilen paket ağırlığı: ");
+                    int weight = scn.nextInt(); //input data from the user
+
+                    //prepareCargoPacket method from the CargoPacket class and sending parameters
                     CargoPacket cp = new CargoPacket();
                     cp.prepareCargoPacket(sevenKgSoap,twoKgSoap,weight);
-
                     break;
 
-                case 2:
+                case 2: //for stop the run
+                    log.info("Çıkış");
                     System.exit(0);
                     break;
 
@@ -60,6 +72,5 @@ public class CalculationApplication {
         }
 
     }
-
 
 }
--- src/main/java/com/example/calculation/CargoPacket.java
+++ src/main/java/com/example/calculation/CargoPacket.java
@@ -1,42 +1,63 @@
 package com.example.calculation;
 
+// importing log4j libraries
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+
 public class CargoPacket
 {
+    private static Logger log = LogManager.getLogger(CalculationApplication.class);
 
     public int prepareCargoPacket(int sevenKgSoapAmount, int twoKgSoapAmount, int weightOfPacket) {
 
+        log.info("7 kg'lık sabun adedi:" + sevenKgSoapAmount);
+        log.info("2 kg'lık sabun adedi:" + twoKgSoapAmount);
+        log.info("İstenen paket ağırlığı" + weightOfPacket);
 
+
+
+        //calculation of the total amount of soap found
         int totalAmountOfSoap = ((sevenKgSoapAmount * 7) + (twoKgSoapAmount * 2));
+        log.info("Mevcut sabun miktarı:" + totalAmountOfSoap + "kg");
 
 
-        if (totalAmountOfSoap < weightOfPacket) {
+        if (totalAmountOfSoap < weightOfPacket) //comparison of package weight with existing soaps
+        {
 
             System.out.println("Maalesef. Elimizde yeterli miktarda sabun yok.");
-
+            log.info("İstenenden az miktarda sabun var.");
             return -1;
         }
-        else
-            {
-            int leftoverFromDivision = (weightOfPacket % 7);
+
+        //package calculation stage if there is enough soap
+        else {
+
+
+            int leftoverFromDivision = (weightOfPacket % 7); //leftover from division with 7
 
             int count;
 
-
-            if ( leftoverFromDivision % 2 == 1)
-            {
-                System.out.println("Maalesef. İstenilen miktara 7 kg ve 2 kg'lık paketlerle ulaşılamıyor.");
+            //2 kg package is not available if the only number remaining from the division
+            if (leftoverFromDivision % 2 == 1) {
+                System.out.println("Maalesef. İstenilen miktara 7 kg'lık ve 2 kg'lık paketlerle ulaşılamıyor.");
+                log.info("İstenilen miktara 7 kg'lık ve 2 kg'lık paketler ile ulaşılamıyor. "
+                        + weightOfPacket + "kg");
                 return -1;
             }
-            else {
 
 
+             else {
+
+                //calculation of the number of packages of 2 kg according to leftover the division process
                 for (count = 0; 2 < leftoverFromDivision; count++) {
 
                     leftoverFromDivision = leftoverFromDivision - 2;
                 }
 
-                int requiredAmountOfTwoKgSoap = (count + 1);
-
+                int requiredAmountOfTwoKgSoap = (count + 1); //Package quantity of at least 2 kg
+                log.info("Kullanılacak en az 2 kg'lık paket sayısı: " + requiredAmountOfTwoKgSoap);
                 System.out.println("Kullanılacak 2 kilogramlık sabun sayısı:" + requiredAmountOfTwoKgSoap);
 
                 return requiredAmountOfTwoKgSoap;
@@ -45,4 +66,5 @@ public class CargoPacket
 
         }
     }
+
 }
